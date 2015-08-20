1dae524ed002c5e99cb178aa99b286ca2288baa5
Julia Poladsky <jpoladsky@gmail.com>
2015-08-20 09:21:55 -0500

    first commiittttttt

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import AppMap from "can-ssr/app-map";
+import route from "can/route/";
+import 'can/map/define/';
+
+const AppViewModel = AppMap.extend({
+  define: {
+    message: {
+      value: 'Hello World!'
+    },
+    title: {
+      value: 'place-my-order',
+      serialize: false
+    }
+  }
+});
+
+export default AppViewModel;
